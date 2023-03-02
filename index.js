d7111f1d290605fafedbb860e583a5b48eba9255
Muhammadsodiq6780 <muxammadsodiqikromjonov7@gmail.com>
2023-03-02 16:24:02 +0500

    4 dars

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-console.log();
+let str = 'Muhammadsodiq'
+let num = 123456
+
+// console.log(num.toString());
+// console.log(num);
+
+console.log(str.slice(-5));
+
+// String length
+// String slice() => slice nechinchi index dan boshlab nechanchi index gacha kesib beradi.
+// String substring() =>
+// String substr()
+// String replace() 
+// String replaceAll()
+// String toUpperCase()
+// String toLowerCase()
+// String concat()
+// String trim()
+// String trimStart()
+// String trimEnd()
+// String padStart()
+// String padEnd()
+// String charAt()
+// String charCodeAt()
+// String split()
---
+++ index1.js
@@ -0,0 +1,104 @@
+// #1
+
+// let a = "ECMAScript";
+
+// if (a == "ECMAScript") {
+//   console.log("Right");
+// } else console.log("You don`t know? ECMAScript");
+
+// #2
+
+// let b = 5;
+
+// if (b > 0) {
+//   console.log(1);
+// } else if (b < 0) {
+//   console.log(-1);
+// } else if (b == 0) {
+//   console.log(0);
+// } else console.log("bunday malumot mavjud emas");
+
+// b > 3 ? console.log(true) : console.log(false);
+
+// #3
+
+// let login = "";
+
+// login == "Employee"
+//   ? console.log("Hello")
+//   : login == "Director"
+//   ? console.log("Greeting")
+//   : login == ""
+//   ? console.log("No login")
+//   : console.log("");
+
+// let result;
+
+// if (a + b < 4) {
+//     result = 'Below'
+// }else result = 'Over'
+
+// let sum = 0
+
+// for(q=1; q < 12; q++){
+//     console.log(sum += q);
+//     if(sum == 10) break
+// }
+
+// let ob_havo = ";lkjhgv";
+
+// switch (ob_havo) {
+//   case "yomgir":
+//     console.log("its raining");
+//     break;
+//   case "qor":
+//     console.log("its snowing");
+
+//     break;
+//   case "quyosh":
+//     console.log("its sunny");
+//     break
+//     default : console.log('malumot yoq');
+
+// let name = "Ilya";
+
+// alert(hello ${"name"} );
+
+// alert(hello ${name} );
+
+// let a=2;
+// let x= 1 + (a *= 2)
+// console.log(x);
+
+// let a=15;
+// let b=117;
+// let c=18;
+
+// if (b>a && b>c){
+//   console.log(b);
+// } else if (a>b && a>c){
+//   console.log(a);
+// } else if (c>a && c>b){
+//   console.log(c);
+// }
+
+// let name="ECMASCRIPT"
+
+// if (name==="ECMASCRIPT")console.log("Correct");
+// else console.log("You are wrong bro");
+
+// let num= "lk"
+
+// if (num>=1)console.log("Musbat son kiritdingiz");
+// else if (num===0) console.log("0 Kiritdingiz");
+// else if (num<=-1) console.log("Manfiy son kiritdingiz");
+// else console.log("Error");
+
+// let x=2
+// let y="g6"
+
+// if (x+y<4)console.log("below");
+// else if (x+y>4) console.log("over");
+// else console.log("error");
+
+   
