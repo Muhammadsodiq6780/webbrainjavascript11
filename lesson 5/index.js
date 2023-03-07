ffddf1f79350addb72cd6f1697de9dfc0b70e8a9
Muhammadsodiq6780 <muxammadsodiqikromjonov7@gmail.com>
2023-03-07 20:04:52 +0500

    lesson 6

---
+++ lesson 5/index.js
@@ -0,0 +1,29 @@
+// let title = "name";
+
+// let bomb = "title";
+
+// let obj = {
+//   title: "xolmat",
+//   //   [title]: "Eshmat",
+//   //   title,
+// };
+// console.log(obj.name);
+
+// console.log(obj[title]);
+// console.log(obj["name"]);
+// console.log(obj.title);
+// console.log("title");
+
+// console.log(Object.keys(obj));
+// title
+// xolmat
+// name
+// let age = "name";
+// let name = "surname";
+// let obj = {
+//   name: "Toshmat",
+//   [surname]: "Eshmatov",
+//   age: 24,
+// };
+// console.log(obj.age);
+// console.log(obj[age]);
